72110d8bef60ec5558bbef184e937b96992ceee4
Magnus Bach Billeskov Brask <magnusbbb@hotmail.com.com>
2024-10-31 12:17:36 +0100

    push push

---
+++ MenuItem.cs
@@ -0,0 +1,11 @@
+﻿namespace IBAS_kantine
+{
+    public class MenuItem
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string Dag { get; set; }
+        public string VarmRet { get; set; }
+        public string KoldRet { get; set; }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,40 @@
-using Microsoft.AspNetCore.Mvc;
+using Azure.Data.Tables;
+using IBAS_kantine;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace IBAS_kantine.Pages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly string connectionString = "DefaultEndpointsProtocol=https;AccountName=kantinestoragereal;AccountKey=OFbRf6vsc2JK4Oi3Frw1lxEYkACDBfWNSbsy69fM43339KUFnnAXbs1YGeBl1ItHioZ3kvhhIvWb+AStly/KQw==;BlobEndpoint=https://kantinestoragereal.blob.core.windows.net/;QueueEndpoint=https://kantinestoragereal.queue.core.windows.net/;TableEndpoint=https://kantinestoragereal.table.core.windows.net/;FileEndpoint=https://kantinestoragereal.file.core.windows.net/;"; 
+    private readonly string tableName = "menuen"; 
+    private TableClient tableClient;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public List<MenuItem> MenuItems { get; set; }
+
+    public IndexModel()
     {
-        _logger = logger;
+        tableClient = new TableClient(connectionString, tableName);
     }
 
-    public void OnGet()
+    public async Task OnGetAsync()
     {
+        MenuItems = new List<MenuItem>();
 
+        // Hent data fra tabellen
+        await foreach (var entity in tableClient.QueryAsync<TableEntity>())
+        {
+            var menuItem = new MenuItem
+            {
+                PartitionKey = entity.PartitionKey,
+                RowKey = entity.RowKey,
+                Dag = entity.ContainsKey("Dag") && entity["Dag"] is string dagValue ? dagValue : null,
+                VarmRet = entity.ContainsKey("VarmRet") && entity["VarmRet"] is string varmRetValue ? varmRetValue : null,
+                KoldRet = entity.ContainsKey("KoldRet") && entity["KoldRet"] is string koldRetValue ? koldRetValue : null
+            };
+            MenuItems.Add(menuItem);
+        }
     }
+
+}
 }
