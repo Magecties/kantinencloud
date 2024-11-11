85235bbda15a7467cf240febad87ca369da25b67
Magnus Bach Billeskov Brask <magnusbbb@hotmail.com.com>
2024-11-11 11:53:02 +0100

    manden

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using Azure.Data.Tables;
+using Azure.Identity; 
 using IBAS_kantine;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
@@ -6,15 +7,15 @@ using System.Threading.Tasks;
 
 public class IndexModel : PageModel
 {
-    private readonly string connectionString = "DefaultEndpointsProtocol=https;AccountName=kantinestoragereal;AccountKey=OFbRf6vsc2JK4Oi3Frw1lxEYkACDBfWNSbsy69fM43339KUFnnAXbs1YGeBl1ItHioZ3kvhhIvWb+AStly/KQw==;BlobEndpoint=https://kantinestoragereal.blob.core.windows.net/;QueueEndpoint=https://kantinestoragereal.queue.core.windows.net/;TableEndpoint=https://kantinestoragereal.table.core.windows.net/;FileEndpoint=https://kantinestoragereal.file.core.windows.net/;"; 
-    private readonly string tableName = "menuen"; 
+    private readonly string tableName = "menuen";
     private TableClient tableClient;
 
     public List<MenuItem> MenuItems { get; set; }
 
     public IndexModel()
     {
-        tableClient = new TableClient(connectionString, tableName);
+        var tableEndpoint = new Uri("https://kantinestoragereal.table.core.windows.net");
+        tableClient = new TableClient(tableEndpoint, tableName, new DefaultAzureCredential());
     }
 
     public async Task OnGetAsync()
@@ -35,6 +36,4 @@ public class IndexModel : PageModel
             MenuItems.Add(menuItem);
         }
     }
-
-}
 }
